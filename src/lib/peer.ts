7b9eb382a523193a349bc1d980f903f89c55fba0
Famxuanlinh <famxuanlinh@gmail.com>
2025-03-20 14:12:03 +0700

    feat: add turn server

--- src/lib/peer.ts
+++ src/lib/peer.ts
@@ -33,10 +33,31 @@ export const peer = new Peer({
   config: {
     iceServers: [
       { urls: 'stun:stun.l.google.com:19302' },
-      { urls: 'stun:stun1.l.google.com:19302' },
-      { urls: 'stun:stun2.l.google.com:19302' },
-      { urls: 'stun:stun3.l.google.com:19302' },
-      { urls: 'stun:stun4.l.google.com:19302' }
+      // { urls: 'stun:stun1.l.google.com:19302' },
+      // { urls: 'stun:stun2.l.google.com:19302' },
+      // { urls: 'stun:stun3.l.google.com:19302' },
+      // { urls: 'stun:stun4.l.google.com:19302' },
+      { urls: 'stun:stun.relay.metered.ca:80' },
+      {
+        urls: 'turn:global.relay.metered.ca:80',
+        username: '49763634d32cb15840386edc',
+        credential: 'G0Jf2GvnWd4IOGj6'
+      },
+      {
+        urls: 'turn:global.relay.metered.ca:80?transport=tcp',
+        username: '49763634d32cb15840386edc',
+        credential: 'G0Jf2GvnWd4IOGj6'
+      },
+      {
+        urls: 'turn:global.relay.metered.ca:443',
+        username: '49763634d32cb15840386edc',
+        credential: 'G0Jf2GvnWd4IOGj6'
+      },
+      {
+        urls: 'turns:global.relay.metered.ca:443?transport=tcp',
+        username: '49763634d32cb15840386edc',
+        credential: 'G0Jf2GvnWd4IOGj6'
+      }
     ],
     iceCandidatePoolSize: 10
   },
