a95a99b2e152b0108c29e5cc6a4f71d664bfedaf
Famxuanlinh <famxuanlinh@gmail.com>
2025-03-27 14:28:33 +0700

    fix: peer error

--- src/app/_components/error-overlay.tsx
+++ src/app/_components/error-overlay.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const ErrorOverlay = ({ message }: { message: string }) => {
+export const ErrorOverlay = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center text-center text-white">
-      <div className="mt-4 text-xs md:text-sm">{message}</div>
+      {/* <div className="mt-4 text-xs opacity-0 md:text-sm">{message}</div> */}
       <button
         onClick={() => window.location.reload()}
         className="mt-6 rounded-full bg-white px-5 py-2 text-xs text-black sm:text-sm"
