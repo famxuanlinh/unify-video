56aab3acdf6e9d1a35796995588ae61cddd9fd49
Famxuanlinh <famxuanlinh@gmail.com>
2025-03-27 09:17:21 +0700

    --amend

--- src/app/_components/jump-in-button.tsx
+++ src/app/_components/jump-in-button.tsx
@@ -16,12 +16,14 @@ export const JumpInButton = () => {
   const isReady = ready && socket?.connect;
 
   const handleJumpIn = () => {
-    if (me) {
-      join();
-    } else {
+    if (!me) {
       toast({
         description: 'Please signin first'
       });
+    }
+
+    if (me && isReady) {
+      join();
     }
   };
 
@@ -29,7 +31,6 @@ export const JumpInButton = () => {
     <div className="absolute left-1/2 z-9 -translate-x-1/2 transform space-x-1 portrait:top-1/2 portrait:-translate-y-1/2 landscape:bottom-20">
       {!started && (
         <button
-          // disabled={!isReady}
           className={`flex cursor-pointer items-center gap-3 rounded-full px-8 py-3 text-[.9rem] font-medium text-black hover:text-black sm:px-6 sm:py-4 sm:text-base ${isReady ? 'bg-[#fffc03] hover:bg-[#fff06b]' : 'bg-gray-300'}`}
           onClick={handleJumpIn}
         >
