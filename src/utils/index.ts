9c407dcfa18e1aa0bb0f1d024d3ea494b39dccab
Famxuanlinh <famxuanlinh@gmail.com>
2025-04-07 23:31:51 +0700

    refactor: remove assets folder

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -25,18 +25,6 @@ export function b64DecodeUnicode(str: string) {
   );
 }
 
-export const toSlug = (text: string): string => {
-  return text
-    .toLowerCase()
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .replace(/[đĐ]/g, 'd')
-    .replace(/([^0-9a-z-\s-_])/g, '')
-    .replace(/(\s+)/g, '_')
-    .replace(/_+/g, '_')
-    .replace(/^-+|-+$/g, '');
-};
-
 export function getImageUrl(hash?: string | null) {
   if (!hash) return undefined;
 
