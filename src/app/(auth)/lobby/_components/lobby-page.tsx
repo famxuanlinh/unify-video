c68a7c2bb236457faecf5e657d5adfb63fe6c92e
Famxuanlinh <famxuanlinh@gmail.com>
2025-04-15 09:13:36 +0700

    feat: call button for lobby page

--- src/app/(auth)/lobby/_components/lobby-page.tsx
+++ src/app/(auth)/lobby/_components/lobby-page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { usePeer } from '@/hooks';
 import { useGetLobbies } from '@/hooks/use-get-lobbies';
+import { Video } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
@@ -20,6 +22,8 @@ import { LobbyCard } from './lobby-card';
 import { LobbyNoData } from './no-data';
 
 export const LobbyPage = () => {
+  const { handleReturnToHome } = usePeer();
+
   const router = useRouter();
   const { data, getLobbies, isLoading, hasMore, offset, reset } =
     useGetLobbies();
@@ -47,7 +51,7 @@ export const LobbyPage = () => {
   const connections = data?.connections || [];
 
   return (
-    <div className="px-4">
+    <div className="relative px-4">
       <div className="pt-1">
         <div className="flex items-center justify-between">
           <div
@@ -99,6 +103,22 @@ export const LobbyPage = () => {
 
         <div ref={ref} className="py-4 text-center">
           {isLoading && <div>Loading...</div>}
+        </div>
+      </div>
+      <div className="sticky right-4 bottom-6">
+        <div className="flex items-center justify-end">
+          <div
+            className="flex h-10 w-10 cursor-pointer items-center justify-center"
+            style={{
+              borderRadius: '49px',
+              background:
+                'linear-gradient(135deg, var(--ORANGE, #FFA941) 0%, var(--RED, #E94057) 100%)',
+              boxShadow: '0px 4px 20px 0px rgba(0, 0, 0, 0.25)'
+            }}
+            onClick={handleReturnToHome}
+          >
+            <Video className="text-white" />
+          </div>
         </div>
       </div>
     </div>
